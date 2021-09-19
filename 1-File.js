04fff3bc416db6f028b3ebb2a70b9320f4110702
Zuhaib <theshahzuhaib96@gmail.com>
2021-09-19 19:37:22 +0530

    wtf did i do with the two sum

--- 1-File.js
+++ 1-File.js
@@ -2,9 +2,16 @@
 const twoSum = function (nums, target) {
 	// from the nums array return indicies of two numbers who sum is target. Assume each input has unique solution
 
+	// for (let i = 0; i < nums.length; i++) {
+	// 	if (nums[i] + nums[i + 1] === target) {
+	// 		return [i, i + 1];
+	// 	}
+	// }
+
 	for (let i = 0; i < nums.length; i++) {
-		if (nums[i] + nums[i + 1] === target) {
-			return [i, i + 1];
+		for (let j = i + 1; j < nums.length; j++) {
+			// i and j are indicies
+			if (nums[i] + nums[j] === target) return [i, j];
 		}
 	}
 };
