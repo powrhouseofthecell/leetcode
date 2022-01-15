fe3c261b8af41fa594d7dc1b26ca78629a44fe88
Zuhaib <theshahzuhaib96@gmail.com>
2022-01-15 18:02:02 +0530

    Add Question Number 2

--- 1-File.js
+++ 1-File.js
@@ -16,8 +16,38 @@ const twoSum = function (nums, target) {
    }
 };
 
-/////////////////////////////////////////////////////////////////////////////
+// +=+=+=+=++=+=+=+=++=+=+=+=++=+=+=+=++=+=+=+=++=+=+=+=++=+=+=+=++=+=+=+=++=+=+=+=+
+// Question Number 2
 
+const addTwoNumbers = function (l1, l2) {
+   let head = new ListNode(0);
+   let cur = head;
+   let curry = 0;
+
+   while (true) {
+      let sum = curry;
+      sum += l1 ? l1.val : 0;
+      sum += l2 ? l2.val : 0;
+      cur.val = sum % 10;
+      curry = parseInt(sum / 10);
+      if (l1) l1 = l1.next;
+      if (l2) l2 = l2.next;
+      if (l1 != null || l2 != null) {
+         cur.next = new ListNode(0);
+         cur = cur.next;
+      } else {
+         break;
+      }
+   }
+   if (curry != 0) {
+      cur.next = new ListNode(0);
+      cur = cur.next;
+      cur.val = curry;
+   }
+   return head;
+};
+
+// +=+=+=+=++=+=+=+=++=+=+=+=++=+=+=+=++=+=+=+=++=+=+=+=++=+=+=+=++=+=+=+=++=+=+=+=+
 // Question 7 ( Reverse an integer )
 // 321  --> 123
 // -123  --> -321
