d7d885a6d3532480df278d2408d77fbc1fb7b732
Zuhaib <theshahzuhaib96@gmail.com>
2022-01-19 20:09:28 +0530

    Q69 Sqrt(x)

---
+++ Q69 - Sqrt(x).js
@@ -0,0 +1,17 @@
+const mySqrt = function (x) {
+   let l = 0,
+      r = x;
+   while (true) {
+      let mid = parseInt(l + (r - l) / 2);
+      if (mid * mid > x) {
+         r = mid - 1;
+      } else if (mid * mid < x) {
+         if ((mid + 1) * (mid + 1) > x) {
+            return mid;
+         }
+         l = mid + 1;
+      } else {
+         return mid;
+      }
+   }
+};
