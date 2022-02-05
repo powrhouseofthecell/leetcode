b87e61fb94154aab0d6e08022fd7585817949274
Zuhaib <theshahzuhaib96@gmail.com>
2022-02-05 12:13:50 +0530

    Prettier: new config

--- Q66 - plusOne.js
+++ Q66 - plusOne.js
@@ -1,16 +1,16 @@
 const plusOne = function (digits) {
-    for (let i = digits.length - 1; i >= 0; i--) {
-        if (digits[i] < 9) {
-            digits[i]++
-            break
-        } else {
-            digits[i] = 0
-        }
-    }
-    if (digits[0] == 0) {
-        digits.unshift(1)
-    }
-    return digits
+   for (let i = digits.length - 1; i >= 0; i--) {
+      if (digits[i] < 9) {
+         digits[i]++;
+         break;
+      } else {
+         digits[i] = 0;
+      }
+   }
+   if (digits[0] == 0) {
+      digits.unshift(1);
+   }
+   return digits;
 };
 
 // console.log([1, 2, 3])
