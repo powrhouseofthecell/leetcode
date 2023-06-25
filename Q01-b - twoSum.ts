4de7daaa55a4ba4e841e62cb109ca219ae743ebe
Zuhaib Nazir <znazir@dc.iquasar.com>
2023-06-25 17:47:58 +0530

    twoSum with hashMap in ts

---
+++ Q01-b - twoSum.ts
@@ -0,0 +1,11 @@
+const twoSumOP = (nums: number[], target: number) => {
+  const hashTable = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (hashTable[target - nums[i]] != undefined) {
+      return [hashTable[target - nums[i]], i];
+    }
+    hashTable[nums[i]] = i;
+  }
+};
+
+console.log(twoSumOP([4, 22, 3, 2], 7));
